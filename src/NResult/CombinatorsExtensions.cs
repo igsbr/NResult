2a1813b316722e19bb9fd0996a2ce7a73c5ceb6d
Igor Logvinov <igsbr@hotmail.com>
2018-06-12 19:38:55 +0700

    add number of classic combinators for the Result<>

---
+++ src/NResult/CombinatorsExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace NResult
+{
+    public static class CombinatorsExtensions
+    {
+        public static Result<U, E> Map<T, U, E>(this IResult<T, E> src, Func<T, U> op)
+        {
+            if (!src.IsOK) return new Result<U, E>(src.Err);
+
+            return new Result<U, E>(op(src.Value));
+        }
+
+        public static Result<U> Map<T, U>(this IResult<T> src, Func<T, U> op)
+        {
+            if (!src.IsOK) return new Result<U>(src.Err);
+
+            return new Result<U>(op(src.Value));
+        }
+
+        public static Result<T, F> MapFail<T, E, F>(this IResult<T, E> src, Func<E, F> op)
+        {
+            if (src.IsOK) return new Result<T, F>(src.Value);
+
+            return new Result<T, F>(op(src.Err));
+        }
+
+        public static Result<T> MapErr<T>(this IResult<T> src, Func<Exception, Exception> op)
+        {
+            if (src.IsOK) return new Result<T>(src.Value);
+
+            return new Result<T>(op(src.Err));
+        }
+
+        public static Result<T, E> And<T, E>(this IResult<T, E> src, Result<T, E> res)
+        {
+            if (!src.IsOK) return new Result<T,E>(src.Err);
+
+            return res;
+        }
+
+        public static Result<T> And<T>(this IResult<T> src, Result<T> res)
+        {
+            if (!src.IsOK) return new Result<T>(src.Err);
+
+            return res;
+        }
+
+        public static Result<T, E> Or<T, E>(this IResult<T, E> src, Result<T, E> res)
+        {
+            if (!src.IsOK) return res;
+
+            return new Result<T, E>(src.Value);
+        }
+
+        public static Result<T> Or<T>(this IResult<T> src, Result<T> res)
+        {
+            if (!src.IsOK) return res;
+
+            return new Result<T>(src.Value);
+        }
+
+        public static Result<U, E> Then<T, U, E>(this IResult<T, E> src, Func<T, Result<U, E>> op)
+        {
+            if (!src.IsOK) return new Result<U, E>(src.Err);
+
+            return op(src.Value);
+        }
+
+        public static Result<U> Then<T, U>(this IResult<T> src, Func<T, Result<U>> op)
+        {
+            if (!src.IsOK) return new Result<U>(src.Err);
+
+            return op(src.Value);
+        }
+
+        public static Result<T, F> Else<T, E, F>(this IResult<T, E> src, Func<E, Result<T, F>> op)
+        {
+            if (src.IsOK) return new Result<T, F>(src.Value);
+
+            return op(src.Err);
+        }
+
+        public static Result<T> Else<T>(this IResult<T> src, Func<Exception, Result<T>> op)
+        {
+            if (src.IsOK) return new Result<T>(src.Value);
+
+            return op(src.Err);
+        }
+    }
+}
---
+++ tests/NResult.Tests/CombinatorsTests.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using Xunit;
+using static NResult.Helpers;
+
+namespace NResult.Tests
+{
+    public class CombinatorsTests
+    {
+        public class MapCombinator
+        {
+            [Fact]
+            public void VerifyOKVariantWithResult_T_E()
+            {
+                var expected = new Result<double, string>(42 * 2.0);
+                var sut = new Result<int, string>(42);
+
+                var actual = sut.Map(i => i * 2.0);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_E()
+            {
+                var expected = new Result<double, string>("foo");
+                var sut = new Result<int, string>("foo");
+
+                var actual = sut.Map(i => i * 2.0);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyOKVariantWithResult_T()
+            {
+                var expected = new Result<double>(42 * 2.0);
+                var sut = new Result<int>(42);
+
+                var actual = sut.Map(i => i * 2.0);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T()
+            {
+                var ex = new Exception();
+                var expected = new Result<double>(ex);
+                var sut = new Result<int>(ex);
+
+                var actual = sut.Map(i => i * 2.0);
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        public class MapErrCombinator
+        {
+            [Fact]
+            public void VerifyOKVariantWithResult_T_E()
+            {
+                var expected = new Result<int, string>(42);
+                var sut = new Result<int, string>(42);
+
+                var actual = sut.MapFail(e => e + "bar");
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_E()
+            {
+                var expected = new Result<int, string>("foobar");
+                var sut = new Result<int, string>("foo");
+
+                var actual = sut.MapFail(e => e + "bar");
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyOKVariantWithResult_T()
+            {
+                var expected = new Result<int>(42);
+                var sut = new Result<int>(42);
+
+                var actual = sut.MapErr(e => new AggregateException(e));
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_()
+            {
+                var ex = new Exception();
+                var sut = new Result<int>(ex);
+
+                var actual = sut.MapErr(e => new AggregateException(e));
+
+                Assert.False(actual.IsOK);
+                Assert.IsType<AggregateException>(actual.Err);
+                Assert.Equal(ex, ((AggregateException)actual.Err).InnerExceptions[0]);
+            }
+        }
+
+        public class AndCombinator
+        {
+            [Fact]
+            public void VerifyOKVariantWithResult_T_E()
+            {
+                var foo = new Result<int, string>(42);
+                var bar = new Result<int, string>(17);
+
+                var actual = foo.And(bar);
+
+                Assert.Equal(bar, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_E()
+            {
+                var foo = new Result<int, string>("wrong");
+                var bar = new Result<int, string>(17);
+
+                var actual = foo.And(bar);
+
+                Assert.Equal(foo, actual);
+            }
+
+            [Fact]
+            public void VerifyOKVariantWithResult_T()
+            {
+                var foo = new Result<int>(42);
+                var bar = new Result<int>(17);
+
+                var actual = foo.And(bar);
+
+                Assert.Equal(bar, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T()
+            {
+                var foo = new Result<int>(new Exception());
+                var bar = new Result<int>(17);
+
+                var actual = foo.And(bar);
+
+                Assert.Equal(foo, actual);
+            }
+        }
+
+        public class OrCombinator
+        {
+            [Fact]
+            public void VerifyOKVariantWithResult_T_E()
+            {
+                var foo = new Result<int, string>(42);
+                var bar = new Result<int, string>(17);
+
+                var actual = foo.Or(bar);
+
+                Assert.Equal(foo, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_E()
+            {
+                var foo = new Result<int, string>("wrong");
+                var bar = new Result<int, string>(17);
+
+                var actual = foo.Or(bar);
+
+                Assert.Equal(bar, actual);
+            }
+
+            [Fact]
+            public void VerifyOKVariantWithResult_T()
+            {
+                var foo = new Result<int>(42);
+                var bar = new Result<int>(17);
+
+                var actual = foo.Or(bar);
+
+                Assert.Equal(foo, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T()
+            {
+                var foo = new Result<int>(new Exception());
+                var bar = new Result<int>(17);
+
+                var actual = foo.Or(bar);
+
+                Assert.Equal(bar, actual);
+            }
+        }
+
+        public class ThenCombinator
+        {
+            [Fact]
+            public void VerifyOKVariantWithResult_T_E()
+            {
+                Result<int, string> Foo() => OK(42);
+                Result<double, string> Bar(int i) => OK(i * 2.0);
+                var expected = new Result<double, string>(42 * 2.0);
+
+                var actual = Foo().Then(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_E()
+            {
+                Result<int, string> Foo() => Fail("wrong");
+                Result<double, string> Bar(int i) => OK(i * 2.0);
+                var expected = new Result<double, string>("wrong");
+
+                var actual = Foo().Then(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyOKVariantWithResult_T()
+            {
+                Result<int> Foo() => OK(42);
+                Result<double> Bar(int i) => OK(i * 2.0);
+                var expected = new Result<double>(42 * 2.0);
+
+                var actual = Foo().Then(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T()
+            {
+                var ex = new Exception();
+
+                Result<int> Foo() => Fail(ex);
+                Result<double> Bar(int i) => OK(i * 2.0);
+                var expected = new Result<double>(ex);
+
+                var actual = Foo().Then(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        public class ElseCombinator
+        {
+            [Fact]
+            public void VerifyOKVariantWithResult_T_E()
+            {
+                Result<int, bool> Foo() => OK(42);
+                Result<int, string> Bar(bool e) => Fail(e.ToString());
+                var expected = new Result<int, string>(42);
+
+                var actual = Foo().Else(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T_E()
+            {
+                Result<int, bool> Foo() => Fail(true);
+                Result<int, string> Bar(bool e) { if (e) return OK(42); else return Fail("wrong"); }
+                var expected = new Result<int, string>(42);
+
+                var actual = Foo().Else(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyOKVariantWithResult_T()
+            {
+                Result<int> Foo() => OK(42);
+                Result<int> Bar(Exception e) => Error(new AggregateException(e));
+                var expected = new Result<int>(42);
+
+                var actual = Foo().Else(Bar);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void VerifyErrVariantWithResult_T()
+            {
+                Result<int> Foo() => Error(new ApplicationException());
+                var expected = new Result<int>(42);
+
+                var actual = Foo().Else(e =>
+                {
+                    if (e is ApplicationException)
+                        return OK(42);
+                    else
+                        return Error(new AggregateException(e, new Exception()));
+                });
+
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
